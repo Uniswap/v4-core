424eb1bbaa3849b4c32f49c23c7324a87cd65621
Moody Salem <moody.salem@gmail.com>
2021-11-11 17:26:55 -0500

    remove unused contracts

--- contracts/libraries/Pool.sol
+++ contracts/libraries/Pool.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.8.9;
 
-import {IPoolEvents} from '../interfaces/pool/IPoolEvents.sol';
-
 import {SafeCast} from './SafeCast.sol';
 import {Tick} from './Tick.sol';
 import {TickBitmap} from './TickBitmap.sol';
