0ff7f6a45a7d1e290e97944d60272d5f6897a133
Lint Action <lint-action@samuelmeuli.com>
2022-01-04 21:08:36 +0000

    Fix code style issues with Prettier

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-import "ds-test/test.sol";
+import 'ds-test/test.sol';
 
 contract ContractTest is DSTest {
     function setUp() public {}
