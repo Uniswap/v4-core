58bb4a1df9b6221798cbfda662a8f0aa45d7bb6d
Moody Salem <moody.salem@gmail.com>
2022-03-05 14:20:25 -0500

    trying to get the tests to run against eip1153 network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,11 +2,31 @@ import 'hardhat-typechain'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-etherscan'
+import { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } from 'hardhat/builtin-tasks/task-names'
+import { subtask } from 'hardhat/config'
+import { TaskArguments } from 'hardhat/types'
+import { join as pathJoin } from 'path'
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args: TaskArguments, hre, runSuper) => {
+  if (args.solcVersion !== '0.8.12') {
+    throw new Error('need binary for solc version')
+  }
+
+  const compilerPath = pathJoin(__dirname, 'bin', 'solc-macosx-amd64-v0.8.12+commit.f00d7308')
+
+  return {
+    compilerPath,
+    isSolcJs: false,
+    version: '0.8.12',
+    longVersion: '0.8.12+commit.f00d7308',
+  }
+})
 
 export default {
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
+      forking: { url: 'https://eip1153.optimism.io/' },
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
