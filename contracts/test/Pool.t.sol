56c03ba5c6b0263c0ae1d4b69fccd81736ea8695
Moody Salem <moody.salem@gmail.com>
2022-05-04 12:01:37 -0400

    modify position fuzz test

--- contracts/test/Pool.t.sol
+++ contracts/test/Pool.t.sol
@@ -12,14 +12,49 @@ contract PoolTest is DSTest {
 
     Pool.State state;
 
-    function testInitialize(uint160 sqrtPriceX96) external {
+    function testInitialize(uint160 sqrtPriceX96) public returns (bool) {
         if (sqrtPriceX96 < TickMath.MIN_SQRT_RATIO || sqrtPriceX96 >= TickMath.MAX_SQRT_RATIO) {
             vm.expectRevert(TickMath.InvalidSqrtRatio.selector);
             state.initialize(sqrtPriceX96);
+            return false;
         } else {
             state.initialize(sqrtPriceX96);
             assertEq(state.slot0.sqrtPriceX96, sqrtPriceX96);
             assertEq(state.slot0.tick, TickMath.getTickAtSqrtRatio(sqrtPriceX96));
+            assertLt(state.slot0.tick, TickMath.MAX_TICK);
+            assertGt(state.slot0.tick, TickMath.MIN_TICK);
+            return true;
         }
+    }
+
+    function testModifyPosition(
+        uint160 sqrtPriceX96,
+        int24 tickLower,
+        int24 tickUpper,
+        int128 liquidityDelta,
+        int24 tickSpacing
+    ) public returns (bool) {
+        if (testInitialize(sqrtPriceX96)) {
+            if (tickLower >= tickUpper) {
+                vm.expectRevert(abi.encodeWithSelector(Pool.TicksMisordered.selector, tickLower, tickUpper));
+            } else if (tickLower < TickMath.MIN_TICK) {
+                vm.expectRevert(abi.encodeWithSelector(Pool.TickLowerOutOfBounds.selector, tickLower));
+            } else if (tickUpper > TickMath.MAX_TICK) {
+                vm.expectRevert(abi.encodeWithSelector(Pool.TickUpperOutOfBounds.selector, tickUpper));
+            }
+
+            state.modifyPosition(
+                Pool.ModifyPositionParams({
+                    owner: address(this),
+                    tickLower: tickLower,
+                    tickUpper: tickUpper,
+                    liquidityDelta: liquidityDelta,
+                    tickSpacing: tickSpacing
+                })
+            );
+
+            return true;
+        }
+        return false;
     }
 }
