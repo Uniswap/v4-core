e72ebe1c37be249561e67f815fbd91ee36b665e6
Alice Henshaw <henshawalice@gmail.com>
2022-05-30 14:06:58 +0100

    remove console logger

--- contracts/libraries/TWAMM/TWAMM.sol
+++ contracts/libraries/TWAMM/TWAMM.sol
@@ -11,7 +11,6 @@ import {FixedPoint96} from '../FixedPoint96.sol';
 import {SqrtPriceMath} from '../SqrtPriceMath.sol';
 import {SwapMath} from '../SwapMath.sol';
 import {SafeCast} from '../SafeCast.sol';
-import 'hardhat/console.sol';
 
 /// @title TWAMM - Time Weighted Average Market Maker
 /// @notice TWAMM represents long term orders in a pool
