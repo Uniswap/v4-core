de1416362076853f98ee47e0a0fd9c068c10ce86
Emily Williams <emag3m@gmail.com>
2022-06-14 17:44:27 -0400

    protocolFeeController accepts key + remove unnecessary poolId function

--- contracts/interfaces/IProtocolFeeController.sol
+++ contracts/interfaces/IProtocolFeeController.sol
@@ -4,5 +4,8 @@ pragma solidity =0.8.13;
 import {IPoolManager} from './IPoolManager.sol';
 
 interface IProtocolFeeController {
-    function protocolFeeForPool(bytes32 id) external view returns (uint8);
+    /// @notice Returns the protocol call fee for a pool given the conditions of this contract
+    /// @param key The pool key to identify the pool. The controller may want to use attributes on the pool
+    ///   to determine the protocol fee, hence the entire key is needed.
+    function protocolFeeForPool(IPoolManager.PoolKey memory key) external view returns (uint8);
 }
