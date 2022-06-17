76387283ba02efd4dfdbe9ae67d2ff47c343674d
Emily Williams <emag3m@gmail.com>
2022-06-17 15:25:11 -0400

    whats going on with snapshots

--- test/SwapMath.spec.ts
+++ test/SwapMath.spec.ts
@@ -230,7 +230,7 @@ describe('SwapMath', () => {
       expect(feeAmount).to.eq(1)
     })
 
-    describe('[ @skip-on-coverage ] gas [ @skip-on-coverage ]', () => {
+    describe('gas [ @skip-on-coverage ]', () => {
       it('swap one for zero exact in capped', async () => {
         await snapshotGasCost(
           swapMath.getGasCostOfComputeSwapStep(
