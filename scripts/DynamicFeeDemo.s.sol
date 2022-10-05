403831d2a4c4fa8d34445f28be10f1d42be8c4aa
Mark Toda <toda.mark@gmail.com>
2022-10-05 13:07:43 -0700

    fix: script deployment and timing-based fee change

--- scripts/DynamicFeeDemo.s.sol
+++ scripts/DynamicFeeDemo.s.sol
@@ -8,11 +8,11 @@ import {TickMath} from '../contracts/libraries/TickMath.sol';
 import {Currency} from '../contracts/libraries/CurrencyLibrary.sol';
 import {PoolModifyPositionTest} from '../contracts/test/PoolModifyPositionTest.sol';
 import {PoolSwapTest} from '../contracts/test/PoolSwapTest.sol';
+import {IHooks} from '../contracts/interfaces/IHooks.sol';
 import {TestERC20} from '../contracts/test/TestERC20.sol';
 import {PoolManager} from '../contracts/PoolManager.sol';
 import {IPoolManager} from '../contracts/interfaces/IPoolManager.sol';
 import {VolatilityOracle} from '../contracts/hooks/VolatilityOracle.sol';
-import {Deployers} from '../test/foundry-tests/utils/Deployers.sol';
 
 contract MyScript is Script {
     function run() external {
@@ -20,28 +20,34 @@ contract MyScript is Script {
         address deployerAddress = vm.addr(deployerKey);
 
         vm.startBroadcast(deployerKey);
+        // SETUP
         uint256 CONTROLLER_FEE = 50000;
-        PoolManager manager = new PoolManager(CONTROLLER_FEE);
-        vm.stopBroadcast();
+        PoolManager manager = new PoolManager{salt: 0x0}(CONTROLLER_FEE);
+        console2.log("Deployed PoolManager", address(manager));
 
-        uint256 pk = 0xf53e3747d93eb10180fc46078e1cf55d9f5fa485ce4106d3eb77404204ca8c25;
-        console2.log(vm.addr(pk));
-        vm.startBroadcast(pk);
-        VolatilityOracle dynamicFeeHook = new VolatilityOracle(manager);
-        vm.stopBroadcast();
+        VolatilityOracle dynamicFeeHook = new VolatilityOracle{salt: 0x4e59b44847b379578588920ca78fbf26c0b4956ca8c62c9e3a9be00000348826}(manager);
+        console2.log("Deployed VolatilityOracle Hook", address(dynamicFeeHook));
 
-        Deployers deployer = new Deployers();
-        vm.startBroadcast(deployerKey);
-
-        uint160 sqrtPriceX96 = 2**96;
-        (IPoolManager.PoolKey memory key, bytes32 id) = deployer.createPool(manager, dynamicFeeHook, sqrtPriceX96);
-        console2.log('Poolmanager', address(manager));
-
-        TestERC20 tokenA = TestERC20(Currency.unwrap(key.currency0));
-        TestERC20 tokenB = TestERC20(Currency.unwrap(key.currency1));
+        TestERC20 tokenA = new TestERC20(10**24);
+        TestERC20 tokenB = new TestERC20(10**24);
         tokenA.mint(deployerAddress, 10**18);
         tokenB.mint(deployerAddress, 10**18);
 
+
+        // INITIALIZE POOL
+        IPoolManager.PoolKey memory key = IPoolManager.PoolKey(
+            Currency.wrap(address(tokenA)),
+            Currency.wrap(address(tokenB)),
+            type(uint24).max,
+            60,
+            IHooks(address(dynamicFeeHook))
+        );
+
+        uint160 sqrtPriceX96 = 2**96;
+        manager.initialize(key, sqrtPriceX96);
+        console2.log("Created pool");
+
+        // ADD LIQUIDITY
         PoolModifyPositionTest addLiquidity = new PoolModifyPositionTest(IPoolManager(address(manager)));
         tokenA.approve(address(addLiquidity), type(uint256).max);
         tokenB.approve(address(addLiquidity), type(uint256).max);
@@ -53,15 +59,18 @@ contract MyScript is Script {
                 liquidityDelta: 10**9
             })
         );
+        console2.log("Added liquidity");
 
         PoolSwapTest swapTest = new PoolSwapTest(manager);
         tokenA.approve(address(swapTest), type(uint256).max);
         tokenB.approve(address(swapTest), type(uint256).max);
 
-        swapTest.swap(key, IPoolManager.SwapParams(false, 100, 2**96 * 2), PoolSwapTest.TestSettings(true, true));
-
-        swapTest.swap(key, IPoolManager.SwapParams(false, 100, 2**96 * 2), PoolSwapTest.TestSettings(true, true));
-
+        // MAKE SWAPS WITH TIMESTAMP INCREASING
+        for (uint256 i = 0; i < 25; i++) {
+            swapTest.swap(key, IPoolManager.SwapParams(false, 100, 2**96 * 2), PoolSwapTest.TestSettings(true, true));
+            console2.log("Swapped");
+            vm.warp(block.timestamp + 100);
+        }
         vm.stopBroadcast();
     }
 }
