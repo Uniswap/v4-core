7355f036448d8bec7dd3c87f8901843c262839a3
Mark Toda <toda.mark@gmail.com>
2023-03-17 11:29:50 -0700

    feat: migrate nodelegatecall tests to forge

---
+++ test/foundry-tests/NoDelegateCall.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {GasSnapshot} from 'forge-gas-snapshot/GasSnapshot.sol';
+import {Test} from 'forge-std/Test.sol';
+import {NoDelegateCallTest} from '../../contracts/test/NoDelegateCallTest.sol';
+
+contract TestDelegateCall is Test, GasSnapshot {
+    error DelegateCallNotAllowed();
+    NoDelegateCallTest noDelegateCallTest;
+
+    function setUp() public {
+        noDelegateCallTest = new NoDelegateCallTest();
+    }
+
+    function testGasOverhead() public {
+        snap(
+            'NoDelegateCallOverhead',
+            noDelegateCallTest.getGasCostOfCannotBeDelegateCalled() -
+                noDelegateCallTest.getGasCostOfCanBeDelegateCalled()
+        );
+    }
+
+    function testDelegateCallNoModifier() public {
+        (bool success, ) = address(noDelegateCallTest).delegatecall(
+            abi.encode(noDelegateCallTest.canBeDelegateCalled.selector)
+        );
+        assertTrue(success);
+    }
+
+    function testDelegateCallWithModifier() public {
+        vm.expectRevert(DelegateCallNotAllowed.selector);
+        (bool success, ) = address(noDelegateCallTest).delegatecall(
+            abi.encode(noDelegateCallTest.cannotBeDelegateCalled.selector)
+        );
+        assertTrue(success);
+    }
+
+    function testCanCallIntoPrivateMethodWithModifier() public view {
+        noDelegateCallTest.callsIntoNoDelegateCallFunction();
+    }
+
+    function testCannotDelegateCallPrivateMethodWithModifier() public {
+        vm.expectRevert(DelegateCallNotAllowed.selector);
+        (bool success, ) = address(noDelegateCallTest).delegatecall(
+            abi.encode(noDelegateCallTest.callsIntoNoDelegateCallFunction.selector)
+        );
+        assertTrue(success);
+    }
+}
