96c19cd03e5349765ada5ab7284702b40d079762
Mark Toda <toda.mark@gmail.com>
2023-03-21 09:42:07 -0700

    fix: forge fmt

--- test/foundry-tests/NoDelegateCall.t.sol
+++ test/foundry-tests/NoDelegateCall.t.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import {GasSnapshot} from 'forge-gas-snapshot/GasSnapshot.sol';
-import {Test} from 'forge-std/Test.sol';
-import {NoDelegateCallTest} from '../../contracts/test/NoDelegateCallTest.sol';
+import {GasSnapshot} from "forge-gas-snapshot/GasSnapshot.sol";
+import {Test} from "forge-std/Test.sol";
+import {NoDelegateCallTest} from "../../contracts/test/NoDelegateCallTest.sol";
 
 contract TestDelegateCall is Test, GasSnapshot {
     error DelegateCallNotAllowed();
+
     NoDelegateCallTest noDelegateCallTest;
 
     function setUp() public {
@@ -15,24 +16,22 @@ contract TestDelegateCall is Test, GasSnapshot {
 
     function testGasOverhead() public {
         snap(
-            'NoDelegateCallOverhead',
-            noDelegateCallTest.getGasCostOfCannotBeDelegateCalled() -
-                noDelegateCallTest.getGasCostOfCanBeDelegateCalled()
+            "NoDelegateCallOverhead",
+            noDelegateCallTest.getGasCostOfCannotBeDelegateCalled()
+                - noDelegateCallTest.getGasCostOfCanBeDelegateCalled()
         );
     }
 
     function testDelegateCallNoModifier() public {
-        (bool success, ) = address(noDelegateCallTest).delegatecall(
-            abi.encode(noDelegateCallTest.canBeDelegateCalled.selector)
-        );
+        (bool success,) =
+            address(noDelegateCallTest).delegatecall(abi.encode(noDelegateCallTest.canBeDelegateCalled.selector));
         assertTrue(success);
     }
 
     function testDelegateCallWithModifier() public {
         vm.expectRevert(DelegateCallNotAllowed.selector);
-        (bool success, ) = address(noDelegateCallTest).delegatecall(
-            abi.encode(noDelegateCallTest.cannotBeDelegateCalled.selector)
-        );
+        (bool success,) =
+            address(noDelegateCallTest).delegatecall(abi.encode(noDelegateCallTest.cannotBeDelegateCalled.selector));
         // note vm.expectRevert inverts success, so a true result here means it reverted
         assertTrue(success);
     }
@@ -43,7 +42,7 @@ contract TestDelegateCall is Test, GasSnapshot {
 
     function testCannotDelegateCallPrivateMethodWithModifier() public {
         vm.expectRevert(DelegateCallNotAllowed.selector);
-        (bool success, ) = address(noDelegateCallTest).delegatecall(
+        (bool success,) = address(noDelegateCallTest).delegatecall(
             abi.encode(noDelegateCallTest.callsIntoNoDelegateCallFunction.selector)
         );
         // note vm.expectRevert inverts success, so a true result here means it reverted
