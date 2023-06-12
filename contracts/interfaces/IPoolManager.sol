9f2c3327dce46c1462c98b88e8ec9af50d48cb63
hensha256 <henshawalice@gmail.com>
2023-06-12 17:04:08 +0200

    Consistent pragma

--- contracts/interfaces/IPoolManager.sol
+++ contracts/interfaces/IPoolManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.6.2;
+pragma solidity ^0.8.19;
 
 import {Currency} from "../libraries/CurrencyLibrary.sol";
 import {Pool} from "../libraries/Pool.sol";
--- contracts/test/MockContract.sol
+++ contracts/test/MockContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.19;
+pragma solidity ^0.8.19;
 
 import {Proxy} from "@openzeppelin/contracts/proxy/Proxy.sol";
 
