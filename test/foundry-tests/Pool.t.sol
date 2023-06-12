2ca48c2034d4d3ff35439c65df73b34ed8b3f254
Mark Toda <toda.mark@gmail.com>
2023-06-12 14:17:59 -0700

    feat: update to 0.8.20
    latest solidity version + some small gas improvements i think mostly
    from push0

--- test/foundry-tests/Pool.t.sol
+++ test/foundry-tests/Pool.t.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
--- test/foundry-tests/utils/TokenFixture.sol
+++ test/foundry-tests/utils/TokenFixture.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import {MockERC20} from "./MockERC20.sol";
 import {Currency} from "../../../contracts/libraries/CurrencyLibrary.sol";
