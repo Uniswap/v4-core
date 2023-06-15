c019b0b3592c58271f4569da81ca1c340c004343
Artem Frantsiian <35114937+ArtemFrantsiian@users.noreply.github.com>
2023-06-15 01:44:14 +0100

    feat: change bytes32 to use PoolId resolving #184 issue (#223)
    * feat: change bytes32 to use PoolId resolving #184 issue
    
    * change poolId to id to be more consistent with PoolKey key
    
    * fix formatting
    
    * fix: remove unused import
    
    ---------
    
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>

--- contracts/test/ProtocolFeeControllerTest.sol
+++ contracts/test/ProtocolFeeControllerTest.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.8.19;
 
 import {IProtocolFeeController} from "../interfaces/IProtocolFeeController.sol";
 import {IPoolManager} from "../interfaces/IPoolManager.sol";
-import {PoolId} from "../libraries/PoolId.sol";
+import {PoolId, PoolIdLibrary} from "../libraries/PoolId.sol";
 
 contract ProtocolFeeControllerTest is IProtocolFeeController {
-    using PoolId for IPoolManager.PoolKey;
+    using PoolIdLibrary for IPoolManager.PoolKey;
 
-    mapping(bytes32 => uint8) public swapFeeForPool;
-    mapping(bytes32 => uint8) public withdrawFeeForPool;
+    mapping(PoolId => uint8) public swapFeeForPool;
+    mapping(PoolId => uint8) public withdrawFeeForPool;
 
     function protocolFeesForPool(IPoolManager.PoolKey memory key) external view returns (uint8, uint8) {
         return (swapFeeForPool[key.toId()], withdrawFeeForPool[key.toId()]);
     }
 
     // for tests to set pool protocol fees
-    function setSwapFeeForPool(bytes32 id, uint8 fee) external {
+    function setSwapFeeForPool(PoolId id, uint8 fee) external {
         swapFeeForPool[id] = fee;
     }
 
-    function setWithdrawFeeForPool(bytes32 id, uint8 fee) external {
+    function setWithdrawFeeForPool(PoolId id, uint8 fee) external {
         withdrawFeeForPool[id] = fee;
     }
 }
--- test/foundry-tests/utils/Deployers.sol
+++ test/foundry-tests/utils/Deployers.sol
@@ -7,11 +7,12 @@ import {Currency} from "../../../contracts/libraries/CurrencyLibrary.sol";
 import {IHooks} from "../../../contracts/interfaces/IHooks.sol";
 import {IPoolManager} from "../../../contracts/interfaces/IPoolManager.sol";
 import {PoolManager} from "../../../contracts/PoolManager.sol";
-import {PoolId} from "../../../contracts/libraries/PoolId.sol";
+import {PoolId, PoolIdLibrary} from "../../../contracts/libraries/PoolId.sol";
 import {Fees} from "../../../contracts/libraries/Fees.sol";
 
 contract Deployers {
     using Fees for uint24;
+    using PoolIdLibrary for IPoolManager.PoolKey;
 
     uint160 constant SQRT_RATIO_1_1 = 79228162514264337593543950336;
     uint160 constant SQRT_RATIO_1_2 = 56022770974786139918731938227;
@@ -27,7 +28,7 @@ contract Deployers {
 
     function createPool(PoolManager manager, IHooks hooks, uint24 fee, uint160 sqrtPriceX96)
         private
-        returns (IPoolManager.PoolKey memory key, bytes32 id)
+        returns (IPoolManager.PoolKey memory key, PoolId id)
     {
         TestERC20[] memory tokens = deployTokens(2, 2 ** 255);
         key = IPoolManager.PoolKey(
@@ -37,13 +38,13 @@ contract Deployers {
             fee.isDynamicFee() ? int24(60) : int24(fee / 100 * 2),
             hooks
         );
-        id = PoolId.toId(key);
+        id = key.toId();
         manager.initialize(key, sqrtPriceX96);
     }
 
     function createFreshPool(IHooks hooks, uint24 fee, uint160 sqrtPriceX96)
         internal
-        returns (PoolManager manager, IPoolManager.PoolKey memory key, bytes32 id)
+        returns (PoolManager manager, IPoolManager.PoolKey memory key, PoolId id)
     {
         manager = createFreshManager();
         (key, id) = createPool(manager, hooks, fee, sqrtPriceX96);
