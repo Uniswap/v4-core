23ebb70df2c9ed0d7849a031c3a3e6e4b90f09d3
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-06-19 18:59:14 +0800

    Revert "Revert "Merge branch 'Uniswap:main' into main""
    This reverts commit af3b4d0b902eabd21e43301b9891a03ca1c701e9.

--- test/foundry-tests/DynamicFees.t.sol
+++ test/foundry-tests/DynamicFees.t.sol
@@ -3,19 +3,13 @@ pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
-import {PoolId} from "../../contracts/libraries/PoolId.sol";
+import {PoolId, PoolIdLibrary} from "../../contracts/libraries/PoolId.sol";
 import {Hooks} from "../../contracts/libraries/Hooks.sol";
-import {MockHooks} from "../../contracts/test/MockHooks.sol";
 import {IPoolManager} from "../../contracts/interfaces/IPoolManager.sol";
-import {TestERC20} from "../../contracts/test/TestERC20.sol";
 import {IHooks} from "../../contracts/interfaces/IHooks.sol";
 import {Currency} from "../../contracts/libraries/CurrencyLibrary.sol";
-import {IERC20Minimal} from "../../contracts/interfaces/external/IERC20Minimal.sol";
 import {PoolManager} from "../../contracts/PoolManager.sol";
-import {SqrtPriceMath} from "../../contracts/libraries/SqrtPriceMath.sol";
-import {PoolModifyPositionTest} from "../../contracts/test/PoolModifyPositionTest.sol";
 import {PoolSwapTest} from "../../contracts/test/PoolSwapTest.sol";
-import {PoolDonateTest} from "../../contracts/test/PoolDonateTest.sol";
 import {Deployers} from "./utils/Deployers.sol";
 import {IDynamicFeeManager} from "././../../contracts/interfaces/IDynamicFeeManager.sol";
 import {Fees} from "./../../contracts/libraries/Fees.sol";
@@ -33,6 +27,8 @@ contract DynamicFees is IDynamicFeeManager {
 }
 
 contract TestDynamicFees is Test, Deployers {
+    using PoolIdLibrary for IPoolManager.PoolKey;
+
     DynamicFees dynamicFees = DynamicFees(
         address(
             uint160(0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF)
@@ -64,7 +60,7 @@ contract TestDynamicFees is Test, Deployers {
     }
 
     event Swap(
-        bytes32 indexed poolId,
+        PoolId indexed poolId,
         address indexed sender,
         int128 amount0,
         int128 amount1,
@@ -77,7 +73,7 @@ contract TestDynamicFees is Test, Deployers {
     function testSwapWorks() public {
         dynamicFees.setFee(123);
         vm.expectEmit(true, true, true, true, address(manager));
-        emit Swap(PoolId.toId(key), address(swapRouter), 0, 0, SQRT_RATIO_1_1 + 1, 0, 0, 123);
+        emit Swap(key.toId(), address(swapRouter), 0, 0, SQRT_RATIO_1_1 + 1, 0, 0, 123);
         swapRouter.swap(
             key, IPoolManager.SwapParams(false, 1, SQRT_RATIO_1_1 + 1), PoolSwapTest.TestSettings(false, false)
         );
