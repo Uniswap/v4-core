94d9e5c9beb0880daea0b17166c9931db1318180
Noah Zinsmeister <noahwz@gmail.com>
2023-07-06 14:43:07 -0400

    use solc 0.8.20

--- contracts/interfaces/callback/ILockCallback.sol
+++ contracts/interfaces/callback/ILockCallback.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 interface ILockCallback {
     /// @notice Called by the pool manager on `msg.sender` when a lock is acquired
--- contracts/test/SqrtPriceMathEchidnaTest.sol
+++ contracts/test/SqrtPriceMathEchidnaTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import {FullMath} from "../libraries/FullMath.sol";
 import {SqrtPriceMath} from "../libraries/SqrtPriceMath.sol";
