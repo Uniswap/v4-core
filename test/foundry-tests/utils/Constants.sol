4ff12835bf4e665e763da86b4d388561acd6c967
Korrrba <gitcoiner@gmail.com>
2023-07-25 10:24:25 +0200

    foundry-test: Refactor constants to a library.
    The constants library will allow to directly access constants from
    multiple Foundry tests.
    
    Ref: #315

---
+++ test/foundry-tests/utils/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+library Constants {
+    uint160 public constant SQRT_RATIO_1_1 = 79228162514264337593543950336;
+    uint160 public constant SQRT_RATIO_1_2 = 56022770974786139918731938227;
+    uint160 public constant SQRT_RATIO_1_4 = 39614081257132168796771975168;
+    uint160 public constant SQRT_RATIO_4_1 = 158456325028528675187087900672;
+}
