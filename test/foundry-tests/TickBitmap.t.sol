56fdbef2b065dc990fd4d8e6418aad4ecc1872df
Korrrba <gitcoiner@gmail.com>
2023-08-07 21:56:17 +0200

    TickBitmap: Remove superfluous console log import.

--- test/foundry-tests/TickBitmap.t.sol
+++ test/foundry-tests/TickBitmap.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "forge-std/console.sol";
 import {Test} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {GasSnapshot} from "../../lib/forge-gas-snapshot/src/GasSnapshot.sol";
