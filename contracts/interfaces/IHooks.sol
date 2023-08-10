d0c754a5fb299c26754da3dc7c3d9c88aea647d2
Emily Williams <emag3m@gmail.com>
2023-08-10 18:00:29 -0400

    data --> hookData + natspec

--- contracts/interfaces/IHooks.sol
+++ contracts/interfaces/IHooks.sol
@@ -14,6 +14,7 @@ interface IHooks {
     /// @param sender The initial msg.sender for the initialize call
     /// @param key The key for the pool being initialized
     /// @param sqrtPriceX96 The sqrt(price) of the pool as a Q64.96
+    /// @param hookData Arbitrary data handed into the PoolManager by the initializer to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function beforeInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96, bytes calldata hookData)
         external
@@ -24,6 +25,7 @@ interface IHooks {
     /// @param key The key for the pool being initialized
     /// @param sqrtPriceX96 The sqrt(price) of the pool as a Q64.96
     /// @param tick The current tick after the state of a pool is initialized
+    /// @param hookData Arbitrary data handed into the PoolManager by the initializer to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function afterInitialize(
         address sender,
@@ -37,37 +39,40 @@ interface IHooks {
     /// @param sender The initial msg.sender for the modify position call
     /// @param key The key for the pool
     /// @param params The parameters for modifying the position
+    /// @param hookData Arbitrary data handed into the PoolManager by the liquiidty provider to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function beforeModifyPosition(
         address sender,
         PoolKey calldata key,
         IPoolManager.ModifyPositionParams calldata params,
-        bytes memory data
+        bytes calldata hookData
     ) external returns (bytes4);
 
     /// @notice The hook called after a position is modified
     /// @param sender The initial msg.sender for the modify position call
     /// @param key The key for the pool
     /// @param params The parameters for modifying the position
+    /// @param hookData Arbitrary data handed into the PoolManager by the liquiidty provider to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function afterModifyPosition(
         address sender,
         PoolKey calldata key,
         IPoolManager.ModifyPositionParams calldata params,
         BalanceDelta delta,
-        bytes memory data
+        bytes calldata hookData
     ) external returns (bytes4);
 
     /// @notice The hook called before a swap
     /// @param sender The initial msg.sender for the swap call
     /// @param key The key for the pool
     /// @param params The parameters for the swap
+    /// @param hookData Arbitrary data handed into the PoolManager by the swapper to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function beforeSwap(
         address sender,
         PoolKey calldata key,
         IPoolManager.SwapParams calldata params,
-        bytes memory data
+        bytes calldata hookData
     ) external returns (bytes4);
 
     /// @notice The hook called after a swap
@@ -75,6 +80,7 @@ interface IHooks {
     /// @param key The key for the pool
     /// @param params The parameters for the swap
     /// @param delta The amount owed to the locker (positive) or owed to the pool (negative)
+    /// @param hookData Arbitrary data handed into the PoolManager by the swapper to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function afterSwap(
         address sender,
@@ -89,6 +95,7 @@ interface IHooks {
     /// @param key The key for the pool
     /// @param amount0 The amount of token0 being donated
     /// @param amount1 The amount of token1 being donated
+    /// @param hookData Arbitrary data handed into the PoolManager by the donor to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function beforeDonate(address sender, PoolKey calldata key, uint256 amount0, uint256 amount1, bytes calldata hookData)
         external
@@ -99,6 +106,7 @@ interface IHooks {
     /// @param key The key for the pool
     /// @param amount0 The amount of token0 being donated
     /// @param amount1 The amount of token1 being donated
+    /// @param hookData Arbitrary data handed into the PoolManager by the donor to be be passed on to the hook
     /// @return bytes4 The function selector for the hook
     function afterDonate(address sender, PoolKey calldata key, uint256 amount0, uint256 amount1, bytes calldata hookData)
         external
