bc651c57040522364fed1cea1598a8f85467f49c
Adrien Husson <adhusson@gmail.com>
2023-08-24 18:03:35 +0200

    Update contracts/types/PoolId.sol
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>

--- contracts/types/PoolId.sol
+++ contracts/types/PoolId.sol
@@ -9,7 +9,7 @@ type PoolId is bytes32;
 library PoolIdLibrary {
     function toId(PoolKey memory poolKey) internal pure returns (PoolId poolId) {
         assembly("memory-safe") {
-            poolId := keccak256(poolKey,mul(32,5))
+            poolId := keccak256(poolKey, mul(32, 5))
         }
     }
 }
