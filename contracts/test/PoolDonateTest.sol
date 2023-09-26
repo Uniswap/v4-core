0095e0848098c3e32e016eac6d2537b67aa47358
saucepoint <98790946+saucepoint@users.noreply.github.com>
2023-09-26 16:00:02 -0400

    squash: support arbitrary calldata on test routers (#361)

--- contracts/test/PoolDonateTest.sol
+++ contracts/test/PoolDonateTest.sol
@@ -24,14 +24,17 @@ contract PoolDonateTest is ILockCallback {
         PoolKey key;
         uint256 amount0;
         uint256 amount1;
+        bytes hookData;
     }
 
-    function donate(PoolKey memory key, uint256 amount0, uint256 amount1)
+    function donate(PoolKey memory key, uint256 amount0, uint256 amount1, bytes memory hookData)
         external
         payable
         returns (BalanceDelta delta)
     {
-        delta = abi.decode(manager.lock(abi.encode(CallbackData(msg.sender, key, amount0, amount1))), (BalanceDelta));
+        delta = abi.decode(
+            manager.lock(abi.encode(CallbackData(msg.sender, key, amount0, amount1, hookData))), (BalanceDelta)
+        );
 
         uint256 ethBalance = address(this).balance;
         if (ethBalance > 0) {
@@ -44,7 +47,7 @@ contract PoolDonateTest is ILockCallback {
 
         CallbackData memory data = abi.decode(rawData, (CallbackData));
 
-        BalanceDelta delta = manager.donate(data.key, data.amount0, data.amount1, new bytes(0));
+        BalanceDelta delta = manager.donate(data.key, data.amount0, data.amount1, data.hookData);
 
         if (delta.amount0() > 0) {
             if (data.key.currency0.isNative()) {
