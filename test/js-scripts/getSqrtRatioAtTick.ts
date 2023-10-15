1191321beb73eca9055140d10312f9bb1bf1f13b
Emily Williams <emag3m@gmail.com>
2023-10-15 15:12:19 -0400

    prettier

--- test/js-scripts/getSqrtRatioAtTick.ts
+++ test/js-scripts/getSqrtRatioAtTick.ts
@@ -1,10 +1,10 @@
 import Decimal from 'decimal.js'
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'
 
-const tickArray = process.argv[2].split(",");
+const tickArray = process.argv[2].split(',')
 const resultsArray = []
 for (let tick of tickArray) {
   const jsResult = new Decimal(1.0001).pow(tick).sqrt().mul(new Decimal(2).pow(96)).toFixed(0)
   resultsArray.push(jsResult)
 }
-process.stdout.write(ethers.utils.defaultAbiCoder.encode(['uint160[]'], [resultsArray]));
+process.stdout.write(ethers.utils.defaultAbiCoder.encode(['uint160[]'], [resultsArray]))
--- test/js-scripts/getTickAtSqrtRatio.ts
+++ test/js-scripts/getTickAtSqrtRatio.ts
@@ -1,10 +1,10 @@
 import Decimal from 'decimal.js'
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'
 
-const sqrtRatioArray = process.argv[2].split(",");
+const sqrtRatioArray = process.argv[2].split(',')
 const resultsArray = []
 for (let sqrtRatio of sqrtRatioArray) {
   const jsResult = new Decimal(sqrtRatio).div(new Decimal(2).pow(96)).pow(2).log(1.0001).floor().toFixed(0)
   resultsArray.push(jsResult)
 }
-process.stdout.write(ethers.utils.defaultAbiCoder.encode(['int256[]'], [resultsArray]));
+process.stdout.write(ethers.utils.defaultAbiCoder.encode(['int256[]'], [resultsArray]))
