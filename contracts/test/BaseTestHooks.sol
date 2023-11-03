ffa197bea281928f11bde8066c6c69c1fc45c8ce
hensha256 <henshawalice@gmail.com>
2023-11-03 14:58:42 +0100

    Fixing compiler warnings

--- contracts/test/BaseTestHooks.sol
+++ contracts/test/BaseTestHooks.sol
@@ -9,78 +9,79 @@ import {IPoolManager} from "../interfaces/IPoolManager.sol";
 contract BaseTestHooks is IHooks {
     error HookNotImplemented();
 
-    function beforeInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96, bytes calldata hookData)
-        external
-        virtual
-        returns (bytes4)
-    {
+    function beforeInitialize(
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        uint160, /* sqrtPriceX96 **/
+        bytes calldata /* hookData **/
+    ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
 
     function afterInitialize(
-        address sender,
-        PoolKey calldata key,
-        uint160 sqrtPriceX96,
-        int24 tick,
-        bytes calldata hookData
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        uint160, /* sqrtPriceX96 **/
+        int24, /* tick **/
+        bytes calldata /* hookData **/
     ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
 
     function beforeModifyPosition(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.ModifyPositionParams calldata params,
-        bytes calldata hookData
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        IPoolManager.ModifyPositionParams calldata, /* params **/
+        bytes calldata /* hookData **/
     ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
 
     function afterModifyPosition(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.ModifyPositionParams calldata params,
-        BalanceDelta delta,
-        bytes calldata hookData
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        IPoolManager.ModifyPositionParams calldata, /* params **/
+        BalanceDelta, /* delta **/
+        bytes calldata /* hookData **/
     ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
 
     function beforeSwap(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.SwapParams calldata params,
-        bytes calldata hookData
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        IPoolManager.SwapParams calldata, /* params **/
+        bytes calldata /* hookData **/
     ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
 
     function afterSwap(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.SwapParams calldata params,
-        BalanceDelta delta,
-        bytes calldata hookData
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        IPoolManager.SwapParams calldata, /* params **/
+        BalanceDelta, /* delta **/
+        bytes calldata /* hookData **/
     ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
 
     function beforeDonate(
-        address sender,
-        PoolKey calldata key,
-        uint256 amount0,
-        uint256 amount1,
-        bytes calldata hookData
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        uint256, /* amount0 **/
+        uint256, /* amount1 **/
+        bytes calldata /* hookData **/
     ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
 
     function afterDonate(
-        address sender,
-        PoolKey calldata key,
-        uint256 amount0,
-        uint256 amount1,
-        bytes calldata hookData
+        address, /* sender **/
+        PoolKey calldata, /* key **/
+        uint256, /* amount0 **/
+        uint256, /* amount1 **/
+        bytes calldata /* hookData **/
     ) external virtual returns (bytes4) {
         revert HookNotImplemented();
     }
