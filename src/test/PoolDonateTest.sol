427116533970460316ee6b3db32cb608780b257e
Alice <34962750+hensha256@users.noreply.github.com>
2023-11-16 17:41:01 +0100

    Part 1: Improve forge tests (#407)
    * Revert messages
    
    * Common take and settle contract for tests
    
    * improving swap and take tests
    
    * add asserts for modify position
    
    * extra asserts in modify position
    
    * asserts in donate test
    
    * More deployment helpers
    
    * native set up in deployer
    
    * simplify initialize tests
    
    * few more corrections
    
    * more cleanup
    
    * remove console logs
    
    * snapshots and test fix post merge
    
    * snapshots
    
    * accidentally pushed foundry.toml
    
    * PR comments

--- src/test/PoolDonateTest.sol
+++ src/test/PoolDonateTest.sol
@@ -2,22 +2,16 @@
 pragma solidity ^0.8.20;
 
 import {Currency, CurrencyLibrary} from "../types/Currency.sol";
-import {IERC20Minimal} from "../interfaces/external/IERC20Minimal.sol";
-
-import {Currency} from "../types/Currency.sol";
-import {ILockCallback} from "../interfaces/callback/ILockCallback.sol";
 import {IPoolManager} from "../interfaces/IPoolManager.sol";
 import {PoolKey} from "../types/PoolKey.sol";
 import {BalanceDelta} from "../types/BalanceDelta.sol";
+import {PoolTestBase} from "./PoolTestBase.sol";
+import {Test} from "forge-std/Test.sol";
 
-contract PoolDonateTest is ILockCallback {
+contract PoolDonateTest is PoolTestBase, Test {
     using CurrencyLibrary for Currency;
 
-    IPoolManager public immutable manager;
-
-    constructor(IPoolManager _manager) {
-        manager = _manager;
-    }
+    constructor(IPoolManager _manager) PoolTestBase(_manager) {}
 
     struct CallbackData {
         address sender;
@@ -47,28 +41,24 @@ contract PoolDonateTest is ILockCallback {
 
         CallbackData memory data = abi.decode(rawData, (CallbackData));
 
+        (,, uint256 reserveBefore0, int256 deltaBefore0) = _fetchBalances(data.key.currency0, data.sender);
+        (,, uint256 reserveBefore1, int256 deltaBefore1) = _fetchBalances(data.key.currency1, data.sender);
+
+        assertEq(deltaBefore0, 0);
+        assertEq(deltaBefore1, 0);
+
         BalanceDelta delta = manager.donate(data.key, data.amount0, data.amount1, data.hookData);
 
-        if (delta.amount0() > 0) {
-            if (data.key.currency0.isNative()) {
-                manager.settle{value: uint128(delta.amount0())}(data.key.currency0);
-            } else {
-                IERC20Minimal(Currency.unwrap(data.key.currency0)).transferFrom(
-                    data.sender, address(manager), uint128(delta.amount0())
-                );
-                manager.settle(data.key.currency0);
-            }
-        }
-        if (delta.amount1() > 0) {
-            if (data.key.currency1.isNative()) {
-                manager.settle{value: uint128(delta.amount1())}(data.key.currency1);
-            } else {
-                IERC20Minimal(Currency.unwrap(data.key.currency1)).transferFrom(
-                    data.sender, address(manager), uint128(delta.amount1())
-                );
-                manager.settle(data.key.currency1);
-            }
-        }
+        (,, uint256 reserveAfter0, int256 deltaAfter0) = _fetchBalances(data.key.currency0, data.sender);
+        (,, uint256 reserveAfter1, int256 deltaAfter1) = _fetchBalances(data.key.currency1, data.sender);
+
+        assertEq(reserveBefore0, reserveAfter0);
+        assertEq(reserveBefore1, reserveAfter1);
+        assertEq(deltaAfter0, int256(data.amount0));
+        assertEq(deltaAfter1, int256(data.amount1));
+
+        if (data.amount0 > 0) _settle(data.key.currency0, data.sender, delta.amount0(), true);
+        if (data.amount1 > 0) _settle(data.key.currency1, data.sender, delta.amount1(), true);
 
         return abi.encode(delta);
     }
