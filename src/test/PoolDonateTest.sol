f3d33eb113ccbd80fc2e45f765d79e3fa054eb68
hensha256 <henshawalice@gmail.com>
2023-12-07 15:41:52 -0500

    fixing tests with fuzzing

--- src/test/PoolDonateTest.sol
+++ src/test/PoolDonateTest.sol
@@ -9,6 +9,7 @@ import {PoolTestBase} from "./PoolTestBase.sol";
 import {Test} from "forge-std/Test.sol";
 import {IHooks} from "../interfaces/IHooks.sol";
 import {Hooks} from "../libraries/Hooks.sol";
+import {console2} from "forge-std/console2.sol";
 
 contract PoolDonateTest is PoolTestBase, Test {
     using CurrencyLibrary for Currency;
@@ -59,24 +60,33 @@ contract PoolDonateTest is PoolTestBase, Test {
         (,, uint256 reserveAfter0, int256 deltaAfter0) = _fetchBalances(data.key.currency0, data.sender);
         (,, uint256 reserveAfter1, int256 deltaAfter1) = _fetchBalances(data.key.currency1, data.sender);
 
+        console2.log("balances fetched");
         if (!data.key.hooks.hasPermissionToAccessLock()) {
+            console2.log("no access lock");
             assertEq(reserveBefore0, reserveAfter0);
             assertEq(reserveBefore1, reserveAfter1);
             if (!data.key.hooks.hasPermissionToNoOp()) {
+                console2.log("no noop");
                 assertEq(deltaAfter0, int256(data.amount0));
                 assertEq(deltaAfter1, int256(data.amount1));
             }
         }
-
+        console2.log("after");
         if (delta == BalanceDeltaLibrary.MAXIMUM_DELTA) {
+            console2.log("max delta");
             // Check that this hook is allowed to NoOp, then we can return as we dont need to settle
             assertTrue(data.key.hooks.hasPermissionToNoOp(), "Invalid NoOp returned");
             return abi.encode(delta);
         }
-
+        console2.log("settles");
         if (deltaAfter0 > 0) _settle(data.key.currency0, data.sender, int128(deltaAfter0), true);
+        console2.log("settle1");
+
         if (deltaAfter1 > 0) _settle(data.key.currency1, data.sender, int128(deltaAfter1), true);
+
+        console2.log("settle2");
         if (deltaAfter0 < 0) _take(data.key.currency0, data.sender, int128(deltaAfter0), true);
+        console2.log("take");
         if (deltaAfter1 < 0) _take(data.key.currency1, data.sender, int128(deltaAfter1), true);
 
         return abi.encode(delta);
