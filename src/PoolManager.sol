6fd0cbb0bce569ed7554bd73712c8dfe47570782
Sara Reynolds <snreynolds2506@gmail.com>
2023-12-07 15:58:53 -0500

    add getters

--- src/PoolManager.sol
+++ src/PoolManager.sol
@@ -21,6 +21,7 @@ import {Claims} from "./Claims.sol";
 import {PoolId, PoolIdLibrary} from "./types/PoolId.sol";
 import {BalanceDelta, BalanceDeltaLibrary} from "./types/BalanceDelta.sol";
 import {Lockers} from "./libraries/Lockers.sol";
+import {PoolGetters} from "./libraries/PoolGetters.sol";
 
 /// @notice Holds the state for all pools
 contract PoolManager is IPoolManager, Fees, NoDelegateCall, Claims {
@@ -31,6 +32,7 @@ contract PoolManager is IPoolManager, Fees, NoDelegateCall, Claims {
     using Position for mapping(bytes32 => Position.Info);
     using CurrencyLibrary for Currency;
     using FeeLibrary for uint24;
+    using PoolGetters for Pool.State;
 
     /// @inheritdoc IPoolManager
     int24 public constant MAX_TICK_SPACING = TickMath.MAX_TICK_SPACING;
@@ -444,6 +446,14 @@ contract PoolManager is IPoolManager, Fees, NoDelegateCall, Claims {
 
     function getCurrentHook() external view returns (IHooks) {
         return Lockers.getCurrentHook();
+    }
+
+    function getPoolTickInfo(PoolId id, int24 tick) external view returns (Pool.TickInfo memory) {
+        return pools[id].getPoolTickInfo(tick);
+    }
+
+    function getPoolBitmapInfo(PoolId id, int16 word) external view returns (uint256 tickBitmap) {
+        return pools[id].getPoolBitmapInfo(word);
     }
 
     /// @notice receive native tokens for native pools
