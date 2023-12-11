ee48cf9cc1bcb95c3a6328c2a39d2a036acf41c1
Eric Zhong <eric.zhong@uniswap.org>
2023-12-11 14:07:09 -0500

    Copy solmate 6909 locally to get interface inheritance

--- src/interfaces/IPoolManager.sol
+++ src/interfaces/IPoolManager.sol
@@ -5,12 +5,13 @@ import {Currency} from "../types/Currency.sol";
 import {PoolKey} from "../types/PoolKey.sol";
 import {Pool} from "../libraries/Pool.sol";
 import {IHooks} from "./IHooks.sol";
+import {IERC6909Claims} from "./external/IERC6909Claims.sol";
 import {IFees} from "./IFees.sol";
 import {BalanceDelta} from "../types/BalanceDelta.sol";
 import {PoolId} from "../types/PoolId.sol";
 import {Position} from "../libraries/Position.sol";
 
-interface IPoolManager is IFees {
+interface IPoolManager is IFees, IERC6909Claims {
     /// @notice Thrown when currencies touched has exceeded max of 256
     error MaxCurrenciesTouched();
 
