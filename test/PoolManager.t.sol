c90b99047aef4385452680179ca474099c2a5446
Eric Zhong <eric.zhong@uniswap.org>
2023-12-11 11:17:22 -0500

    fix fialing test

--- test/PoolManager.t.sol
+++ test/PoolManager.t.sol
@@ -48,7 +48,7 @@ contract PoolManagerTest is Test, Deployers, GasSnapshot {
         int24 tick,
         uint24 fee
     );
-    event ProtocolFeeUpdated(PoolId indexed id, uint24 protocolFees);
+    event ProtocolFeeUpdated(PoolId indexed id, uint16 protocolFee);
     event Transfer(
         address caller, address indexed sender, address indexed receiver, uint256 indexed id, uint256 amount
     );
