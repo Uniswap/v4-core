b8a333b0d85b2fdd21767804c754c1622bbc35c6
soham <22412996+zemse@users.noreply.github.com>
2024-02-12 23:30:10 +0530

    Make pool getters internal (#457)
    Co-authored-by: Alice <34962750+hensha256@users.noreply.github.com>

--- src/libraries/PoolGetters.sol
+++ src/libraries/PoolGetters.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.20;
 import {Pool} from "./Pool.sol";
 
 library PoolGetters {
-    function getPoolTickInfo(Pool.State storage pool, int24 tick) external view returns (Pool.TickInfo memory) {
+    function getPoolTickInfo(Pool.State storage pool, int24 tick) internal view returns (Pool.TickInfo memory) {
         return pool.ticks[tick];
     }
 
-    function getPoolBitmapInfo(Pool.State storage pool, int16 word) external view returns (uint256 tickBitmap) {
+    function getPoolBitmapInfo(Pool.State storage pool, int16 word) internal view returns (uint256 tickBitmap) {
         return pool.tickBitmap[word];
     }
 }
