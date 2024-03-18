39af6329cbb2c9fa6a372850ef5462cb77576993
hensha256 <henshawalice@gmail.com>
2024-03-18 11:23:49 -0300

    Hardcode constants

--- src/libraries/Lock.sol
+++ src/libraries/Lock.sol
@@ -6,8 +6,8 @@ import {IHooks} from "../interfaces/IHooks.sol";
 /// @notice This is a temporary library that allows us to use transient storage (tstore/tload)
 /// TODO: This library can be deleted when we have the transient keyword support in solidity.
 library Lock {
-    // The slot holding the lock, transiently
-    uint256 constant LOCK_SLOT = uint256(keccak256("Lock")) - 1;
+    // The slot holding the lock, transiently. uint256(keccak256("Lock")) - 1;
+    uint256 constant LOCK_SLOT = uint256(0x3e061da06d9da7c1d2a497a320750161d58a0fc1d9a008f9c4f3d1606155d86e);
 
     function lock() internal {
         uint256 slot = LOCK_SLOT;
