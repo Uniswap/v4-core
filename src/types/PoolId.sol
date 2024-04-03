27acc2599644f6788dc48f8749e92b81a4901b44
Adrien Husson <adhusson@gmail.com>
2024-04-03 20:26:49 +0200

    [copy of #340] perf: optimize poolKey.toId() (skip abi.encode) (#550)
    * perf: optimize PoolKey.toId() (skip abi.encode)
    
    * Update contracts/types/PoolId.sol
    
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
    
    * Update test/foundry-tests/types/PoolKey.t.sol
    
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
    
    * doc: explain toId optim + comment-style assembly guard
    
    * Update contracts/types/PoolId.sol
    
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
    
    * Merge branch 'main' into perf/optimize-toId
    
    * remove redundant PoolKey.t.sol
    
    ---------
    
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>

--- src/types/PoolId.sol
+++ src/types/PoolId.sol
@@ -7,7 +7,11 @@ type PoolId is bytes32;
 
 /// @notice Library for computing the ID of a pool
 library PoolIdLibrary {
-    function toId(PoolKey memory poolKey) internal pure returns (PoolId) {
-        return PoolId.wrap(keccak256(abi.encode(poolKey)));
+    /// @notice Returns value equal to keccak256(abi.encode(poolKey))
+    function toId(PoolKey memory poolKey) internal pure returns (PoolId poolId) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            poolId := keccak256(poolKey, mul(32, 5))
+        }
     }
 }
