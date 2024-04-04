a21afe1c9ddbe48df55d7fe9ceb8d3ed657a1f68
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-04-04 04:46:21 -0400

    Optimize `TickBitmap`

--- src/libraries/TickBitmap.sol
+++ src/libraries/TickBitmap.sol
@@ -12,14 +12,29 @@ library TickBitmap {
     /// @param tickSpacing The tick spacing of the pool
     error TickMisaligned(int24 tick, int24 tickSpacing);
 
+    /// @dev round towards negative infinity
+    function compress(int24 tick, int24 tickSpacing) internal pure returns (int24 compressed) {
+        // compressed = tick / tickSpacing;
+        // if (tick < 0 && tick % tickSpacing != 0) compressed--;
+        assembly {
+            compressed :=
+                sub(
+                    sdiv(tick, tickSpacing),
+                    // if (tick < 0 && tick % tickSpacing != 0) then tick % tickSpacing < 0, vice versa
+                    slt(smod(tick, tickSpacing), 0)
+                )
+        }
+    }
+
     /// @notice Computes the position in the mapping where the initialized bit for a tick lives
     /// @param tick The tick for which to compute the position
     /// @return wordPos The key in the mapping containing the word in which the bit is stored
     /// @return bitPos The bit position in the word where the flag is stored
     function position(int24 tick) internal pure returns (int16 wordPos, uint8 bitPos) {
-        unchecked {
-            wordPos = int16(tick >> 8);
-            bitPos = uint8(int8(tick % 256));
+        assembly {
+            // signed arithmetic shift right
+            wordPos := sar(8, tick)
+            bitPos := and(tick, 0xff)
         }
     }
 
@@ -28,11 +43,18 @@ library TickBitmap {
     /// @param tick The tick to flip
     /// @param tickSpacing The spacing between usable ticks
     function flipTick(mapping(int16 => uint256) storage self, int24 tick, int24 tickSpacing) internal {
-        unchecked {
-            if (tick % tickSpacing != 0) revert TickMisaligned(tick, tickSpacing); // ensure that the tick is spaced
-            (int16 wordPos, uint8 bitPos) = position(tick / tickSpacing);
-            uint256 mask = 1 << bitPos;
-            self[wordPos] ^= mask;
+        if (tick % tickSpacing != 0) revert TickMisaligned(tick, tickSpacing); // ensure that the tick is spaced
+        assembly ("memory-safe") {
+            tick := sdiv(tick, tickSpacing)
+            // calculate the storage slot corresponding to the tick
+            // wordPos = tick >> 8
+            mstore(0, sar(8, tick))
+            mstore(0x20, self.slot)
+            // the slot of self[wordPos] is keccak256(abi.encode(wordPos, self.slot))
+            let slot := keccak256(0, 0x40)
+            // mask = 1 << bitPos = 1 << (tick % 256)
+            // self[wordPos] ^= mask
+            sstore(slot, xor(sload(slot), shl(and(tick, 0xff), 1)))
         }
     }
 
@@ -50,35 +72,63 @@ library TickBitmap {
         int24 tickSpacing,
         bool lte
     ) internal view returns (int24 next, bool initialized) {
-        unchecked {
-            int24 compressed = tick / tickSpacing;
-            if (tick < 0 && tick % tickSpacing != 0) compressed--; // round towards negative infinity
+        int24 compressed = compress(tick, tickSpacing);
+        uint256 masked;
 
-            if (lte) {
-                (int16 wordPos, uint8 bitPos) = position(compressed);
-                // all the 1s at or to the right of the current bitPos
-                uint256 mask = (1 << bitPos) - 1 + (1 << bitPos);
-                uint256 masked = self[wordPos] & mask;
+        if (lte) {
+            (int16 wordPos, uint8 bitPos) = position(compressed);
+            // all the 1s at or to the right of the current bitPos
+            // mask = (1 << (bitPos + 1)) - 1
+            // (bitPos + 1) may be 256 but fine
+            // masked = self[wordPos] & mask
+            assembly ("memory-safe") {
+                mstore(0, wordPos)
+                mstore(0x20, self.slot)
+                let mask := sub(shl(add(bitPos, 1), 1), 1)
+                masked := and(sload(keccak256(0, 0x40)), mask)
+                initialized := gt(masked, 0)
+            }
 
-                // if there are no initialized ticks to the right of or at the current tick, return rightmost in the word
-                initialized = masked != 0;
-                // overflow/underflow is possible, but prevented externally by limiting both tickSpacing and tick
-                next = initialized
-                    ? (compressed - int24(uint24(bitPos - BitMath.mostSignificantBit(masked)))) * tickSpacing
-                    : (compressed - int24(uint24(bitPos))) * tickSpacing;
+            // if there are no initialized ticks to the right of or at the current tick, return rightmost in the word
+            // overflow/underflow is possible, but prevented externally by limiting both tickSpacing and tick
+            if (initialized) {
+                uint8 msb = BitMath.mostSignificantBit(masked);
+                assembly {
+                    next := mul(add(sub(compressed, bitPos), msb), tickSpacing)
+                }
             } else {
-                // start from the word of the next tick, since the current tick state doesn't matter
-                (int16 wordPos, uint8 bitPos) = position(compressed + 1);
-                // all the 1s at or to the left of the bitPos
-                uint256 mask = ~((1 << bitPos) - 1);
-                uint256 masked = self[wordPos] & mask;
+                assembly {
+                    next := mul(sub(compressed, bitPos), tickSpacing)
+                }
+            }
+        } else {
+            // start from the word of the next tick, since the current tick state doesn't matter
+            unchecked {
+                ++compressed;
+            }
+            (int16 wordPos, uint8 bitPos) = position(compressed);
+            // all the 1s at or to the left of the bitPos
+            // mask = ~((1 << bitPos) - 1)
+            // masked = self[wordPos] & mask
+            assembly ("memory-safe") {
+                mstore(0, wordPos)
+                mstore(0x20, self.slot)
+                let mask := not(sub(shl(bitPos, 1), 1))
+                masked := and(sload(keccak256(0, 0x40)), mask)
+                initialized := gt(masked, 0)
+            }
 
-                // if there are no initialized ticks to the left of the current tick, return leftmost in the word
-                initialized = masked != 0;
-                // overflow/underflow is possible, but prevented externally by limiting both tickSpacing and tick
-                next = initialized
-                    ? (compressed + 1 + int24(uint24(BitMath.leastSignificantBit(masked) - bitPos))) * tickSpacing
-                    : (compressed + 1 + int24(uint24(type(uint8).max - bitPos))) * tickSpacing;
+            // if there are no initialized ticks to the left of the current tick, return leftmost in the word
+            // overflow/underflow is possible, but prevented externally by limiting both tickSpacing and tick
+            if (initialized) {
+                uint8 lsb = BitMath.leastSignificantBit(masked);
+                assembly {
+                    next := mul(add(sub(compressed, bitPos), lsb), tickSpacing)
+                }
+            } else {
+                assembly {
+                    next := mul(add(sub(compressed, bitPos), 255), tickSpacing)
+                }
             }
         }
     }
