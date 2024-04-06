b57d12d6b67090274caae4f72b9e18dd94909bde
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-04-06 17:53:17 -0400

    Add `FullMath.mulDivQ128` and refactor `Position.update`
    A new function named `mulDivQ128` has been added to `FullMath`. This function calculates x * y / 2^128 with full precision. The calls to `mulDiv` in `Position.sol` were updated to use this new function. The changes also resulted in a decrease in gas in multiple snapshots. Tests were added to validate the functionality of `mulDivQ128`.

--- src/libraries/Position.sol
+++ src/libraries/Position.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import {FullMath} from "./FullMath.sol";
-import {FixedPoint128} from "./FixedPoint128.sol";
 import {LiquidityMath} from "./LiquidityMath.sol";
 
 /// @title Position
@@ -69,10 +68,8 @@ library Position {
 
         // calculate accumulated fees. overflow in the subtraction of fee growth is expected
         unchecked {
-            feesOwed0 =
-                FullMath.mulDiv(feeGrowthInside0X128 - self.feeGrowthInside0LastX128, liquidity, FixedPoint128.Q128);
-            feesOwed1 =
-                FullMath.mulDiv(feeGrowthInside1X128 - self.feeGrowthInside1LastX128, liquidity, FixedPoint128.Q128);
+            feesOwed0 = FullMath.mulDivQ128(feeGrowthInside0X128 - self.feeGrowthInside0LastX128, liquidity);
+            feesOwed1 = FullMath.mulDivQ128(feeGrowthInside1X128 - self.feeGrowthInside1LastX128, liquidity);
         }
 
         // update the position
