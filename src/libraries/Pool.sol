7a1394425bb2aa4822dbeed83ceb72d07ed9ad17
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-04-06 02:27:58 -0400

    Optimize liquidity flipping calculation in `updateTick`
    Updated the way liquidity flipping is calculated in 'Pool.sol' to use bitwise XOR operations for efficiency. This modification improved overall bytecode size in multiple snapshots which demonstrates an optimization of gas usage in our smart contracts.

--- src/libraries/Pool.sol
+++ src/libraries/Pool.sol
@@ -532,7 +532,10 @@ library Pool {
 
         liquidityGrossAfter = LiquidityMath.addDelta(liquidityGrossBefore, liquidityDelta);
 
-        flipped = (liquidityGrossAfter == 0) != (liquidityGrossBefore == 0);
+        // Equivalent to `flipped = (liquidityGrossAfter == 0) != (liquidityGrossBefore == 0);`
+        assembly {
+            flipped := xor(iszero(liquidityGrossAfter), iszero(liquidityGrossBefore))
+        }
 
         if (liquidityGrossBefore == 0) {
             // by convention, we assume that all growth before a tick was initialized happened _below_ the tick
