607a189140f983b4f077115dedc97fd800f58a2c
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-04-09 17:02:58 -0400

    Remove gas getter in `LiquidityMathTest`

--- src/test/LiquidityMathTest.sol
+++ src/test/LiquidityMathTest.sol
@@ -7,10 +7,4 @@ contract LiquidityMathTest {
     function addDelta(uint128 x, int128 y) external pure returns (uint128 z) {
         return LiquidityMath.addDelta(x, y);
     }
-
-    function getGasCostOfAddDelta(uint128 x, int128 y) external view returns (uint256) {
-        uint256 gasBefore = gasleft();
-        LiquidityMath.addDelta(x, y);
-        return gasBefore - gasleft();
-    }
 }
