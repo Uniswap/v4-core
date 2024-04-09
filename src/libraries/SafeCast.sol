3cd89aa470e2b0da03adc6c22caa8abbd01b9add
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-04-08 23:04:06 -0400

    Optimize `SafeCast` with inline assembly
    Refactor `SafeCast.sol` library using inline assembly for better memory-safe operations. By using inline assembly, the code optimizes casting operations which result in reduced gas usage. These changes affect numerous `.forge-snapshot` files, demonstrating an overall reduction in gas usage across multiple contracts.

--- src/libraries/SafeCast.sol
+++ src/libraries/SafeCast.sol
@@ -10,8 +10,15 @@ library SafeCast {
     /// @param y The uint256 to be downcasted
     /// @return z The downcasted integer, now type uint160
     function toUint160(uint256 y) internal pure returns (uint160 z) {
-        z = uint160(y);
-        if (z != y) revert SafeCastOverflow();
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(lt(y, shl(160, 1))) {
+                // revert SafeCastOverflow();
+                mstore(0, 0x93dafdf1)
+                revert(0x1c, 0x04)
+            }
+            z := y
+        }
     }
 
     /// @notice Cast a uint256 to a uint128, revert on overflow
@@ -33,23 +40,44 @@ library SafeCast {
     /// @param y The int256 to be downcasted
     /// @return z The downcasted integer, now type int128
     function toInt128(int256 y) internal pure returns (int128 z) {
-        z = int128(y);
-        if (z != y) revert SafeCastOverflow();
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(eq(y, signextend(15, y))) {
+                // revert SafeCastOverflow();
+                mstore(0, 0x93dafdf1)
+                revert(0x1c, 0x04)
+            }
+            z := y
+        }
     }
 
     /// @notice Cast a uint256 to a int256, revert on overflow
     /// @param y The uint256 to be casted
     /// @return z The casted integer, now type int256
     function toInt256(uint256 y) internal pure returns (int256 z) {
-        if (y > uint256(type(int256).max)) revert SafeCastOverflow();
-        z = int256(y);
+        /// @solidity memory-safe-assembly
+        assembly {
+            if slt(y, 0) {
+                // revert SafeCastOverflow();
+                mstore(0, 0x93dafdf1)
+                revert(0x1c, 0x04)
+            }
+            z := y
+        }
     }
 
     /// @notice Cast a uint256 to a int128, revert on overflow
     /// @param y The uint256 to be downcasted
     /// @return z The downcasted integer, now type int128
     function toInt128(uint256 y) internal pure returns (int128 z) {
-        if (y > uint128(type(int128).max)) revert SafeCastOverflow();
-        z = int128(int256(y));
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(lt(y, shl(127, 1))) {
+                // revert SafeCastOverflow();
+                mstore(0, 0x93dafdf1)
+                revert(0x1c, 0x04)
+            }
+            z := y
+        }
     }
 }
