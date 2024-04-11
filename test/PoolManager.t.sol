43fa6c32ecdfdab712ec8e6f76dabd2f48a6b98e
diana <diana.kocsis@uniswap.org>
2024-04-11 17:16:59 -0400

    change protocol fee push to be via protocol fee controller (#577)
    * change protocol fee push to be via protocol fee controller
    
    * change comment
    
    * out of bounds fee controller change

--- test/PoolManager.t.sol
+++ test/PoolManager.t.sol
@@ -762,8 +762,8 @@ contract PoolManagerTest is Test, Deployers, GasSnapshot {
 
         uint24 protocolFee = (uint24(protocolFee1) << 12) | uint24(protocolFee0);
 
-        feeController.setProtocolFeeForPool(key.toId(), protocolFee);
-        manager.setProtocolFee(key);
+        vm.prank(address(feeController));
+        manager.setProtocolFee(key, protocolFee);
 
         (,, uint24 slot0ProtocolFee,) = manager.getSlot0(key.toId());
         assertEq(slot0ProtocolFee, protocolFee);
@@ -954,42 +954,38 @@ contract PoolManagerTest is Test, Deployers, GasSnapshot {
     function test_setProtocolFee_updatesProtocolFeeForInitializedPool(uint24 protocolFee) public {
         (,, uint24 slot0ProtocolFee,) = manager.getSlot0(key.toId());
         assertEq(slot0ProtocolFee, 0);
-        feeController.setProtocolFeeForPool(key.toId(), protocolFee);
 
         uint16 fee0 = protocolFee.getZeroForOneFee();
         uint16 fee1 = protocolFee.getOneForZeroFee();
+        vm.prank(address(feeController));
         if ((fee0 > 2500) || (fee1 > 2500)) {
-            vm.expectRevert(IProtocolFees.ProtocolFeeControllerCallFailedOrInvalidResult.selector);
-            manager.setProtocolFee(key);
+            vm.expectRevert(IProtocolFees.InvalidProtocolFee.selector);
+            manager.setProtocolFee(key, protocolFee);
         } else {
             vm.expectEmit(false, false, false, true);
             emit IProtocolFees.ProtocolFeeUpdated(key.toId(), protocolFee);
-            manager.setProtocolFee(key);
+            manager.setProtocolFee(key, protocolFee);
 
             (,, slot0ProtocolFee,) = manager.getSlot0(key.toId());
             assertEq(slot0ProtocolFee, protocolFee);
         }
     }
 
-    function test_setProtocolFee_failsWithInvalidProtocolFeeControllers() public {
+    function test_setProtocolFee_failsWithInvalidFee() public {
         (,, uint24 slot0ProtocolFee,) = manager.getSlot0(key.toId());
         assertEq(slot0ProtocolFee, 0);
 
-        manager.setProtocolFeeController(revertingFeeController);
-        vm.expectRevert(IProtocolFees.ProtocolFeeControllerCallFailedOrInvalidResult.selector);
-        manager.setProtocolFee(key);
+        vm.prank(address(feeController));
+        vm.expectRevert(IProtocolFees.InvalidProtocolFee.selector);
+        manager.setProtocolFee(key, MAX_FEE_BOTH_TOKENS + 1);
+    }
 
-        manager.setProtocolFeeController(outOfBoundsFeeController);
-        vm.expectRevert(IProtocolFees.ProtocolFeeControllerCallFailedOrInvalidResult.selector);
-        manager.setProtocolFee(key);
+    function test_setProtocolFee_failsWithInvalidCaller() public {
+        (,, uint24 slot0ProtocolFee,) = manager.getSlot0(key.toId());
+        assertEq(slot0ProtocolFee, 0);
 
-        manager.setProtocolFeeController(overflowFeeController);
-        vm.expectRevert(IProtocolFees.ProtocolFeeControllerCallFailedOrInvalidResult.selector);
-        manager.setProtocolFee(key);
-
-        manager.setProtocolFeeController(invalidReturnSizeFeeController);
-        vm.expectRevert(IProtocolFees.ProtocolFeeControllerCallFailedOrInvalidResult.selector);
-        manager.setProtocolFee(key);
+        vm.expectRevert(IProtocolFees.InvalidCaller.selector);
+        manager.setProtocolFee(key, MAX_FEE_BOTH_TOKENS);
     }
 
     function test_collectProtocolFees_initializesWithProtocolFeeIfCalled() public {
@@ -1008,8 +1004,9 @@ contract PoolManagerTest is Test, Deployers, GasSnapshot {
     function test_collectProtocolFees_ERC20_accumulateFees_gas() public {
         uint256 expectedFees = 7;
 
-        feeController.setProtocolFeeForPool(key.toId(), MAX_FEE_BOTH_TOKENS);
-        manager.setProtocolFee(key);
+        uint24 protocolFee = MAX_FEE_BOTH_TOKENS;
+        vm.prank(address(feeController));
+        manager.setProtocolFee(key, protocolFee);
 
         (,, uint24 slot0ProtocolFee,) = manager.getSlot0(key.toId());
         assertEq(slot0ProtocolFee, MAX_FEE_BOTH_TOKENS);
@@ -1035,8 +1032,9 @@ contract PoolManagerTest is Test, Deployers, GasSnapshot {
     function test_collectProtocolFees_ERC20_returnsAllFeesIf0IsProvidedAsParameter() public {
         uint256 expectedFees = 7;
 
-        feeController.setProtocolFeeForPool(key.toId(), MAX_FEE_BOTH_TOKENS);
-        manager.setProtocolFee(key);
+        uint24 protocolFee = MAX_FEE_BOTH_TOKENS;
+        vm.prank(address(feeController));
+        manager.setProtocolFee(key, protocolFee);
 
         (,, uint24 slot0ProtocolFee,) = manager.getSlot0(key.toId());
         assertEq(slot0ProtocolFee, MAX_FEE_BOTH_TOKENS);
@@ -1062,8 +1060,9 @@ contract PoolManagerTest is Test, Deployers, GasSnapshot {
         Currency nativeCurrency = CurrencyLibrary.NATIVE;
 
         // set protocol fee before initializing the pool as it is fetched on initialization
-        feeController.setProtocolFeeForPool(nativeKey.toId(), MAX_FEE_BOTH_TOKENS);
-        manager.setProtocolFee(nativeKey);
+        uint24 protocolFee = MAX_FEE_BOTH_TOKENS;
+        vm.prank(address(feeController));
+        manager.setProtocolFee(nativeKey, protocolFee);
 
         (,, uint24 slot0ProtocolFee,) = manager.getSlot0(nativeKey.toId());
         assertEq(slot0ProtocolFee, MAX_FEE_BOTH_TOKENS);
@@ -1090,8 +1089,9 @@ contract PoolManagerTest is Test, Deployers, GasSnapshot {
         uint256 expectedFees = 7;
         Currency nativeCurrency = CurrencyLibrary.NATIVE;
 
-        feeController.setProtocolFeeForPool(nativeKey.toId(), MAX_FEE_BOTH_TOKENS);
-        manager.setProtocolFee(nativeKey);
+        uint24 protocolFee = MAX_FEE_BOTH_TOKENS;
+        vm.prank(address(feeController));
+        manager.setProtocolFee(nativeKey, protocolFee);
 
         (,, uint24 slot0ProtocolFee,) = manager.getSlot0(nativeKey.toId());
         assertEq(slot0ProtocolFee, MAX_FEE_BOTH_TOKENS);
