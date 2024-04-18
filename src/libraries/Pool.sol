15cdb4b7b512b871456bb3604544a0f715f1834a
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-04-18 02:09:10 -0400

    Switch if-else branches for gas savings
    Reversed the order of if-else operations in the Pool.sol file, which results in improved gas efficiency. This change mainly involves the 'zeroForOne' and 'exactInput' conditions and comes in response to attempted simplification and gas reduction optimization. Minor fluctuations in snapshots reflect this code change and gas savings effectiveness.

--- src/libraries/Pool.sol
+++ src/libraries/Pool.sol
@@ -436,22 +436,22 @@ library Pool {
         if (cache.liquidityStart != state.liquidity) self.liquidity = state.liquidity;
 
         // update fee growth global
-        if (zeroForOne) {
-            self.feeGrowthGlobal0X128 = state.feeGrowthGlobalX128;
-        } else {
+        if (!zeroForOne) {
             self.feeGrowthGlobal1X128 = state.feeGrowthGlobalX128;
+        } else {
+            self.feeGrowthGlobal0X128 = state.feeGrowthGlobalX128;
         }
 
         unchecked {
-            if (zeroForOne == exactInput) {
-                result = toBalanceDelta(
-                    (params.amountSpecified - state.amountSpecifiedRemaining).toInt128(),
-                    state.amountCalculated.toInt128()
-                );
-            } else {
+            if (zeroForOne != exactInput) {
                 result = toBalanceDelta(
                     state.amountCalculated.toInt128(),
                     (params.amountSpecified - state.amountSpecifiedRemaining).toInt128()
+                );
+            } else {
+                result = toBalanceDelta(
+                    (params.amountSpecified - state.amountSpecifiedRemaining).toInt128(),
+                    state.amountCalculated.toInt128()
                 );
             }
         }
