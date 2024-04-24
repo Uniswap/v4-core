80c12e0a07b49e9e5d9e4b2afa294b416fd347a0
Diana Kocsis <diana.kocsis@uniswap.org>
2024-04-24 13:39:40 -0400

    use relative pathing

--- test/libraries/SafeCast.t.sol
+++ test/libraries/SafeCast.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
-import {SafeCast} from "src/libraries/SafeCast.sol";
+import {SafeCast} from "../../src/libraries/SafeCast.sol";
 
 contract SafeCastTest is Test {
     function test_fuzz_toUint160(uint256 x) public {
