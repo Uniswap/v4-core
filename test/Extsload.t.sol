dafe9a698e6d6a0fcdb3dee4e447078134c0ca9e
Philippe Dumonet <philippe@dumo.net>
2024-04-28 22:48:55 -0400

    add spare extsload

---
+++ test/Extsload.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {Extsload} from "../src/Extsload.sol";
+
+contract Loadable is Extsload {}
+
+/// @author philogy <https://github.com/philogy>
+contract ExtsloadTest is Test {
+    Loadable loadable = new Loadable();
+
+    function test_load10_sparse() public {
+        bytes32[] memory keys = new bytes32[](10);
+        for (uint256 i = 0; i < keys.length; i++) {
+            keys[i] = keccak256(abi.encode(i));
+            vm.store(address(loadable), keys[i], bytes32(i));
+        }
+
+        bytes32[] memory values = loadable.extsload(keys);
+        assertEq(values.length, keys.length);
+        for (uint256 i = 0; i < values.length; i++) {
+            assertEq(values[i], bytes32(i));
+        }
+    }
+}
