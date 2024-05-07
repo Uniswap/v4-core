2e512430730cd820cdf3ec59f218ebe63fac479e
hensha256 <henshawalice@gmail.com>
2024-05-07 17:07:53 -0400

    natspec

--- src/interfaces/IPoolManager.sol
+++ src/interfaces/IPoolManager.sol
@@ -182,7 +182,7 @@ interface IPoolManager is IProtocolFees, IERC6909Claims, IExtsload {
     /// @return swapDelta The balance delta of the address swapping
     /// @dev Swapping on low liquidity pools may cause unexpected swap amounts when liquidity available is less than amountSpecified.
     /// Additionally note that if interacting with hooks that have the BEFORE_SWAP_RETURNS_DELTA_FLAG or AFTER_SWAP_RETURNS_DELTA_FLAG
-    /// the hook may take input and/or output tokens from the user. Integrators should perform checks on the returned swapDelta.
+    /// the hook may alter the swap input/output. Integrators should perform checks on the returned swapDelta.
     function swap(PoolKey memory key, SwapParams memory params, bytes calldata hookData)
         external
         returns (BalanceDelta);
