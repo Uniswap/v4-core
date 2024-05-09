19ecb4c113347c3fa0c97a9ed5ba08f8f97570db
gretzke <daniel@gretzke.de>
2024-05-09 17:26:47 +0200

    adjust natspec for exttload

--- src/interfaces/IExttload.sol
+++ src/interfaces/IExttload.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.8.24;
 
 interface IExttload {
-    /// @notice Called by external contracts to access granular pool state
-    /// @param slot Key of slot to sload
+    /// @notice Called by external contracts to access transient storage of the contract
+    /// @param slot Key of slot to tload
     /// @return value The value of the slot as bytes32
     function exttload(bytes32 slot) external view returns (bytes32 value);
 }
