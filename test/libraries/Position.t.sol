bf9164487f3d4e8827b606d5bc6724b8d8ddd84e
Diana Kocsis <diana.kocsis@uniswap.org>
2024-05-10 15:48:56 -0400

    fix import

--- test/libraries/Position.t.sol
+++ test/libraries/Position.t.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
-import {Position} from "src/libraries/Position.sol";
+import {Position} from "../../src/libraries/Position.sol";
 
 contract PositionTest is Test {
     using Position for mapping(bytes32 => Position.Info);
 
     mapping(bytes32 => Position.Info) internal positions;
 
-    function test_get_fuzz(address owner, int24 tickLower, int24 tickUpper, bytes32 salt) public view {
+    function test_fuzz_get(address owner, int24 tickLower, int24 tickUpper, bytes32 salt) public view {
         bytes32 positionKey = keccak256(abi.encodePacked(owner, tickLower, tickUpper, salt));
         Position.Info storage expectedPosition = positions[positionKey];
         Position.Info storage position = positions.get(owner, tickLower, tickUpper, salt);
