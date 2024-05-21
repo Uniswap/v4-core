c586043c15a682f237d11e4ca6bca6bdaabf0230
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-05-20 23:19:33 -0400

    Optimize contiguous `extsload` variant

--- src/Extsload.sol
+++ src/Extsload.sol
@@ -16,16 +16,22 @@ abstract contract Extsload is IExtsload {
 
     /// @inheritdoc IExtsload
     function extsload(bytes32 startSlot, uint256 nSlots) external view returns (bytes memory) {
-        bytes memory value = new bytes(32 * nSlots);
-
         /// @solidity memory-safe-assembly
         assembly {
-            for { let i := 0 } lt(i, nSlots) { i := add(i, 1) } {
-                mstore(add(value, mul(add(i, 1), 32)), sload(add(startSlot, i)))
+            // The abi offset of dynamic array in the returndata is 32.
+            mstore(0, 0x20)
+            // A left bit-shift of 5 is equivalent to multiplying by 32 but costs less gas.
+            mstore(0x20, shl(5, nSlots))
+            let end := add(0x40, shl(5, nSlots))
+            for { let memptr := 0x40 } 1 {} {
+                mstore(memptr, sload(startSlot))
+                memptr := add(memptr, 0x20)
+                startSlot := add(startSlot, 1)
+                if iszero(lt(memptr, end)) { break }
             }
+            // The end offset is also the length of the returndata.
+            return(0, end)
         }
-
-        return value;
     }
 
     /// @inheritdoc IExtsload
