ab6598f15c219b0969e962e0aa714ff50025afe9
Sara Reynolds <snreynolds2506@gmail.com>
2024-05-24 12:50:41 -0400

    rename CurrencySettleTake, move to test

--- src/test/CustomCurveHook.sol
+++ src/test/CustomCurveHook.sol
@@ -9,7 +9,7 @@ import {PoolKey} from "../types/PoolKey.sol";
 import {BeforeSwapDelta, toBeforeSwapDelta} from "../types/BeforeSwapDelta.sol";
 import {BalanceDelta} from "../types/BalanceDelta.sol";
 import {Currency} from "../types/Currency.sol";
-import {CurrencySettleTake} from "../libraries/CurrencySettleTake.sol";
+import {CurrencySettler} from "../../test/utils/CurrencySettler.sol";
 import {BaseTestHooks} from "./BaseTestHooks.sol";
 import {IERC20Minimal} from "../interfaces/external/IERC20Minimal.sol";
 import {CurrencyLibrary, Currency} from "../types/Currency.sol";
@@ -17,7 +17,7 @@ import {CurrencyLibrary, Currency} from "../types/Currency.sol";
 contract CustomCurveHook is BaseTestHooks {
     using Hooks for IHooks;
     using CurrencyLibrary for Currency;
-    using CurrencySettleTake for Currency;
+    using CurrencySettler for Currency;
 
     error AddLiquidityDirectToHook();
 
