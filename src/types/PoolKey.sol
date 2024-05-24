db7a460ab49abdeaed19faeb02bf6364cac17e10
saucepoint <saucepoint@protonmail.com>
2024-05-24 12:44:10 -0400

    update natspec

--- src/types/PoolKey.sol
+++ src/types/PoolKey.sol
@@ -10,7 +10,7 @@ struct PoolKey {
     Currency currency0;
     /// @notice The higher currency of the pool, sorted numerically
     Currency currency1;
-    /// @notice The pool swap fee, capped at 1_000_000. If the first bit is 1, the pool has a dynamic fee.
+    /// @notice The pool swap fee, capped at 1_000_000. If the first bit is 1, the pool has a dynamic fee and must be exactly equal to 0x800000
     uint24 fee;
     /// @notice Ticks that involve positions must be a multiple of tick spacing
     int24 tickSpacing;
