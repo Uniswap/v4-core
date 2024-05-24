2362b30ca54416a3ae6658865e77aa3f659c2f12
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-05-24 01:17:44 -0400

    Refactor hook call to use inline assembly
    Updated the `callHook` function in Hooks.sol to use low-level calls with inline assembly, improving code efficiency. Also removed the `_revert` private method due to redundancy. As a result, the bytecode size has reduced in several .forge-snapshot files.

--- src/libraries/Hooks.sol
+++ src/libraries/Hooks.sol
@@ -127,9 +127,27 @@ library Hooks {
     /// @notice performs a hook call using the given calldata on the given hook that doesnt return a delta
     /// @return result The complete data returned by the hook
     function callHook(IHooks self, bytes memory data) internal returns (bytes memory result) {
-        bool success;
-        (success, result) = address(self).call(data);
-        if (!success) _revert(result);
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(call(gas(), self, 0, add(data, 0x20), mload(data), 0, 0)) {
+                if iszero(returndatasize()) {
+                    // if the call failed without a revert reason, revert with `FailedHookCall()`
+                    mstore(0, 0x36bc48c5)
+                    revert(0x1c, 0x04)
+                }
+                // bubble up revert
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+            // allocate result byte array from the free memory pointer
+            result := mload(0x40)
+            // store new free memory pointer at the end of the array padded to 32 bytes
+            mstore(0x40, add(result, and(add(returndatasize(), 0x3f), not(0x1f))))
+            // store length in memory
+            mstore(result, returndatasize())
+            // copy return data to result
+            returndatacopy(add(result, 0x20), 0, returndatasize())
+        }
 
         // Check expected selector and returned selector match.
         if (result.parseSelector() != data.parseSelector()) InvalidHookResponse.selector.revertWith();
@@ -326,13 +344,5 @@ library Hooks {
 
     function hasPermission(IHooks self, uint160 flag) internal pure returns (bool) {
         return uint160(address(self)) & flag != 0;
-    }
-
-    /// @notice bubble up revert if present. Else throw FailedHookCall
-    function _revert(bytes memory result) private pure {
-        if (result.length == 0) FailedHookCall.selector.revertWith();
-        assembly {
-            revert(add(0x20, result), mload(result))
-        }
     }
 }
