bf06c56f8a876e26a857c49b195bd74cba2604b9
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-05-24 07:21:25 -0500

    Use `abi.encodeCall` in place of `abi.encodeWithSelector` (#704)
    * Use `abi.encodeCall` in `ProtocolFees`
    
    The refactor replaces `abi.encodeWithSelector` with `abi.encodeCall` in the `protocolFeeForPool` method of `ProtocolFees.sol`. This has resulted in a reduced snapshot size for both the pool manager and initialization bytecode.
    
    * Use `abi.encodeCall` in `Hooks.afterModifyLiquidity`
    
    * Use `abi.encodeCall` in `beforeSwap` and `afterSwap`

--- src/ProtocolFees.sol
+++ src/ProtocolFees.sol
@@ -69,7 +69,7 @@ abstract contract ProtocolFees is IProtocolFees, Owned {
             if (gasleft() < controllerGasLimit) ProtocolFeeCannotBeFetched.selector.revertWith();
 
             (bool _success, bytes memory _data) = address(protocolFeeController).call{gas: controllerGasLimit}(
-                abi.encodeWithSelector(IProtocolFeeController.protocolFeeForPool.selector, key)
+                abi.encodeCall(IProtocolFeeController.protocolFeeForPool, (key))
             );
             // Ensure that the return data fits within a word
             if (!_success || _data.length > 32) return (false, 0);
