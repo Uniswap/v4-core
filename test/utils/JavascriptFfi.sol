55ad729caff405fa672cde64f0e26198b0269ca6
gretzke <daniel@gretzke.de>
2024-05-27 00:34:29 +0200

    remove console logs

--- test/utils/JavascriptFfi.sol
+++ test/utils/JavascriptFfi.sol
@@ -3,14 +3,9 @@ pragma solidity ^0.8.20;
 
 import {CommonBase} from "forge-std/Base.sol";
 
-import "forge-std/console2.sol";
-
 abstract contract JavascriptFfi is CommonBase {
     function runScript(string memory scriptName, string memory args) internal returns (bytes memory result) {
         string[] memory inputs = new string[](8);
-
-        console2.log(scriptName);
-        console2.log(args);
 
         // build ffi command string
         inputs[0] = "npm";
