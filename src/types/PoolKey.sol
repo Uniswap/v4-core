ad0c616868ed9ad331c7846067b196f72c2f5b8b
Junion <junionreal@gmail.com>
2024-06-18 18:31:51 -0400

    Add poolkey library

--- src/types/PoolKey.sol
+++ src/types/PoolKey.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Currency} from "./Currency.sol";
 import {IHooks} from "../interfaces/IHooks.sol";
+import {IPoolManager} from "./../interfaces/IPoolManager.sol";
 
 /// @notice Returns the key for identifying a pool
 struct PoolKey {
@@ -16,4 +17,26 @@ struct PoolKey {
     int24 tickSpacing;
     /// @notice The hooks of the pool
     IHooks hooks;
+}
+
+library PoolKeyLibrary {
+    /// @notice Returns the unspecified currency and amount for a swap
+    function getUnspecified(PoolKey calldata key, IPoolManager.SwapParams calldata params)
+        internal
+        pure
+        returns (Currency currencyUnspecified, int256 amountUnspecified)
+    {
+        bool currency0Specified = (params.amountSpecified < 0 == params.zeroForOne);
+        return (currency0Specified) ? (key.currency1, params.amountSpecified) : (key.currency0, params.amountSpecified);
+    }
+
+    /// @notice Returns the specified currency and amount for a swap
+    function getSpecified(PoolKey calldata key, IPoolManager.SwapParams calldata params)
+        internal
+        pure
+        returns (Currency currencySpecified, int256 amountSpecified)
+    {
+        bool currency0Specified = (params.amountSpecified < 0 == params.zeroForOne);
+        return (currency0Specified) ? (key.currency0, params.amountSpecified) : (key.currency1, params.amountSpecified);
+    }
 }
