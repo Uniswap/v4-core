b642ee1777c69539da3caf37b0497a2198002e9d
Alice Henshaw <henshawalice@gmail.com>
2024-07-11 17:53:04 +0200

    extsload and exttload fix

--- src/Extsload.sol
+++ src/Extsload.sol
@@ -25,6 +25,7 @@ abstract contract Extsload is IExtsload {
             mstore(memptr, 0x20)
             // Store the length of the bytes returned
             mstore(add(memptr, 0x20), length)
+            // update memptr to the first location to hold a result
             memptr := add(memptr, 0x40)
             let end := add(memptr, length)
             for {} 1 {} {
@@ -33,7 +34,6 @@ abstract contract Extsload is IExtsload {
                 startSlot := add(startSlot, 1)
                 if iszero(lt(memptr, end)) { break }
             }
-            // The end offset is also the length of the returndata.
             return(start, sub(end, start))
         }
     }
@@ -41,20 +41,22 @@ abstract contract Extsload is IExtsload {
     /// @inheritdoc IExtsload
     function extsload(bytes32[] calldata slots) external view returns (bytes32[] memory) {
         assembly ("memory-safe") {
+            let memptr := mload(0x40)
+            let start := memptr
             // Copy the abi offset of dynamic array and the length of the array to memory.
-            calldatacopy(0, 0x04, 0x40)
+            calldatacopy(memptr, 0x04, 0x40)
+            // update memptr to the first location to hold a result
+            memptr := add(memptr, 0x40)
             // A left bit-shift of 5 is equivalent to multiplying by 32 but costs less gas.
-            let end := add(0x40, shl(5, slots.length))
+            let end := add(memptr, shl(5, slots.length))
             let calldataptr := slots.offset
-            // Return values will start at 64 while calldata offset is 68.
-            for { let memptr := 0x40 } 1 {} {
+            for {} 1 {} {
                 mstore(memptr, sload(calldataload(calldataptr)))
                 memptr := add(memptr, 0x20)
                 calldataptr := add(calldataptr, 0x20)
                 if iszero(lt(memptr, end)) { break }
             }
-            // The end offset is also the length of the returndata.
-            return(0, end)
+            return(start, sub(end, start))
         }
     }
 }
