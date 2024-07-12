133fd12b581db4236f1ecaa8ee5d5a29ef5777e7
Alice Henshaw <henshawalice@gmail.com>
2024-07-12 19:16:12 +0200

    CVF 24, 25, 26

--- src/libraries/SafeCast.sol
+++ src/libraries/SafeCast.sol
@@ -17,28 +17,26 @@ library SafeCast {
 
     /// @notice Cast a uint256 to a uint160, revert on overflow
     /// @param x The uint256 to be downcasted
-    /// @return The downcasted integer, now type uint160
-    function toUint160(uint256 x) internal pure returns (uint160) {
-        if (x >= 1 << 160) _revertOverflow();
-        return uint160(x);
+    /// @return y The downcasted integer, now type uint160
+    function toUint160(uint256 x) internal pure returns (uint160 y) {
+        y = uint160(x);
+        if (y != x) _revertOverflow();
     }
 
     /// @notice Cast a uint256 to a uint128, revert on overflow
     /// @param x The uint256 to be downcasted
-    /// @return The downcasted integer, now type uint128
-    function toUint128(uint256 x) internal pure returns (uint128) {
-        if (x >= 1 << 128) _revertOverflow();
-        return uint128(x);
+    /// @return y The downcasted integer, now type uint128
+    function toUint128(uint256 x) internal pure returns (uint128 y) {
+        y = uint128(x);
+        if (x != y) _revertOverflow();
     }
 
     /// @notice Cast a int256 to a int128, revert on overflow or underflow
     /// @param x The int256 to be downcasted
-    /// @return The downcasted integer, now type int128
-    function toInt128(int256 x) internal pure returns (int128) {
-        unchecked {
-            if (((1 << 127) + uint256(x)) >> 128 == uint256(0)) return int128(x);
-            _revertOverflow();
-        }
+    /// @return y The downcasted integer, now type int128
+    function toInt128(int256 x) internal pure returns (int128 y) {
+        y = int128(x);
+        if (y != x) _revertOverflow();
     }
 
     /// @notice Cast a uint256 to a int256, revert on overflow
