24049480b3d6c08d7f44f6c82ba7320b93d626ce
Alice Henshaw <henshawalice@gmail.com>
2024-07-12 19:28:02 +0200

    cvf 27

--- src/libraries/SafeCast.sol
+++ src/libraries/SafeCast.sol
@@ -1,26 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.20;
 
+import {CustomRevert} from "./CustomRevert.sol";
+
 /// @title Safe casting methods
 /// @notice Contains methods for safely casting between types
 library SafeCast {
-    error SafeCastOverflow();
+    using CustomRevert for bytes4;
 
-    function _revertOverflow() private pure {
-        assembly ("memory-safe") {
-            // Store the function selector of `SafeCastOverflow()`.
-            mstore(0x00, 0x93dafdf1)
-            // Revert with (offset, size).
-            revert(0x1c, 0x04)
-        }
-    }
+    error SafeCastOverflow();
 
     /// @notice Cast a uint256 to a uint160, revert on overflow
     /// @param x The uint256 to be downcasted
     /// @return y The downcasted integer, now type uint160
     function toUint160(uint256 x) internal pure returns (uint160 y) {
         y = uint160(x);
-        if (y != x) _revertOverflow();
+        if (y != x) SafeCastOverflow.selector.revertWith();
     }
 
     /// @notice Cast a uint256 to a uint128, revert on overflow
@@ -28,7 +23,7 @@ library SafeCast {
     /// @return y The downcasted integer, now type uint128
     function toUint128(uint256 x) internal pure returns (uint128 y) {
         y = uint128(x);
-        if (x != y) _revertOverflow();
+        if (x != y) SafeCastOverflow.selector.revertWith();
     }
 
     /// @notice Cast a int256 to a int128, revert on overflow or underflow
@@ -36,22 +31,22 @@ library SafeCast {
     /// @return y The downcasted integer, now type int128
     function toInt128(int256 x) internal pure returns (int128 y) {
         y = int128(x);
-        if (y != x) _revertOverflow();
+        if (y != x) SafeCastOverflow.selector.revertWith();
     }
 
     /// @notice Cast a uint256 to a int256, revert on overflow
     /// @param x The uint256 to be casted
-    /// @return The casted integer, now type int256
-    function toInt256(uint256 x) internal pure returns (int256) {
-        if (int256(x) >= 0) return int256(x);
-        _revertOverflow();
+    /// @return y The casted integer, now type int256
+    function toInt256(uint256 x) internal pure returns (int256 y) {
+        y = int256(x);
+        if (y < 0) SafeCastOverflow.selector.revertWith();
     }
 
     /// @notice Cast a uint256 to a int128, revert on overflow
     /// @param x The uint256 to be downcasted
     /// @return The downcasted integer, now type int128
     function toInt128(uint256 x) internal pure returns (int128) {
-        if (x >= 1 << 127) _revertOverflow();
+        if (x >= 1 << 127) SafeCastOverflow.selector.revertWith();
         return int128(int256(x));
     }
 }
