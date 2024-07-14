3f3b4bcab3314e5731e6372ce77e8a4c3fe54e25
Alice Henshaw <henshawalice@gmail.com>
2024-07-14 13:46:55 +0100

    Custom error library

--- src/types/Currency.sol
+++ src/types/Currency.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {IERC20Minimal} from "../interfaces/external/IERC20Minimal.sol";
+import {CustomRevert} from "../libraries/CustomRevert.sol";
 
 type Currency is address;
 
@@ -27,6 +28,8 @@ function greaterThanOrEqualTo(Currency currency, Currency other) pure returns (b
 /// @title CurrencyLibrary
 /// @dev This library allows for transferring and holding native tokens and ERC20 tokens
 library CurrencyLibrary {
+    using CustomRevert for bytes4;
+
     /// @notice Thrown when a native transfer fails
     error NativeTransferFailed();
 
@@ -45,7 +48,7 @@ library CurrencyLibrary {
                 // Transfer the ETH and revert if it fails.
                 success := call(gas(), to, amount, 0, 0, 0, 0)
             }
-            if (!success) revert NativeTransferFailed();
+            if (!success) NativeTransferFailed.selector.revertWith();
         } else {
             assembly ("memory-safe") {
                 // Get a pointer to some free memory.
@@ -73,7 +76,7 @@ library CurrencyLibrary {
                 mstore(add(fmp, 0x20), 0) // 4 bytes of `to` and 28 bytes of `amount` were stored here
                 mstore(add(fmp, 0x40), 0) // 4 bytes of `amount` were stored here
             }
-            if (!success) revert ERC20TransferFailed();
+            if (!success) ERC20TransferFailed.selector.revertWith();
         }
     }
 
