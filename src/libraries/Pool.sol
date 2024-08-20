0ec8314a2ff73acc5a2a816aff15bb2a99e2889e
dianakocsis <diana.kocsis@uniswap.org>
2024-08-20 18:13:11 -0400

    trail of bits i01

--- src/libraries/Pool.sol
+++ src/libraries/Pool.sol
@@ -309,7 +309,7 @@ library Pool {
 
         bool exactInput = params.amountSpecified < 0;
 
-        if (swapFee == LPFeeLibrary.MAX_LP_FEE && !exactInput) {
+        if (swapFee >= LPFeeLibrary.MAX_LP_FEE && !exactInput) {
             InvalidFeeForExactOut.selector.revertWith();
         }
 
