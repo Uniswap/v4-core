815034c0567c5fcc72133506a12b4a7bba8bee82
Alice <34962750+hensha256@users.noreply.github.com>
2024-08-30 13:21:27 +0100

    Update src/interfaces/IPoolManager.sol
    Co-authored-by: saucepoint <98790946+saucepoint@users.noreply.github.com>

--- src/interfaces/IPoolManager.sol
+++ src/interfaces/IPoolManager.sol
@@ -169,7 +169,7 @@ interface IPoolManager is IProtocolFees, IERC6909Claims, IExtsload, IExttload {
     /// Donors should keep this in mind when designing donation mechanisms.
     /// @dev This function donates to in-range LPs at slot0.tick. In certain edge-cases of the swap algorithm, the `sqrtPrice` of
     /// a pool can be at the lower boundary of tick `n`, but the `slot0.tick` of the pool is already `n - 1`. In this case a call to
-    /// `donate` would donate to tick `n - 1` not `n`. Read the comments in `Pool.swap()` for more information about this.
+    /// `donate` would donate to tick `n - 1` (slot0.tick) not tick `n` (getTickAtSqrtPrice(slot0.sqrtPriceX96)). Read the comments in `Pool.swap()` for more information about this.
     /// @param key The key of the pool to donate to
     /// @param amount0 The amount of currency0 to donate
     /// @param amount1 The amount of currency1 to donate
