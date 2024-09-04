617b5aedbb69617f6dce13110297559f2d8b3a29
Sara Reynolds <snreynolds2506@gmail.com>
2024-09-04 17:05:28 -0400

    remove import

--- test/libraries/BipsLibrary.t.sol
+++ test/libraries/BipsLibrary.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
-import "forge-std/StdError.sol";
 import {BipsLibrary} from "../../src/libraries/BipsLibrary.sol";
 
 contract BipsLibraryTest is Test {
