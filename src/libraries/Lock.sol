1deea0e2c09dfe9032f37f3e0bd0daeaaf70d51b
MarcusWentz <markzcollege404@gmail.com>
2024-10-22 11:18:29 -0700

    Update Lock.sol in Solidity 0.8.28 for transient variable support

--- src/libraries/Lock.sol
+++ src/libraries/Lock.sol
@@ -1,28 +1,18 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.24;
+pragma solidity 0.8.28;
 
-/// @notice This is a temporary library that allows us to use transient storage (tstore/tload)
-/// TODO: This library can be deleted when we have the transient keyword support in solidity.
-library Lock {
-    // The slot holding the unlocked state, transiently. bytes32(uint256(keccak256("Unlocked")) - 1)
-    bytes32 internal constant IS_UNLOCKED_SLOT = 0xc090fc4683624cfc3884e9d8de5eca132f2d0ec062aff75d43c0465d5ceeab23;
+contract Lock {
+    bool internal transient IS_UNLOCKED_SLOT;
 
     function unlock() internal {
-        assembly ("memory-safe") {
-            // unlock
-            tstore(IS_UNLOCKED_SLOT, true)
-        }
+        IS_UNLOCKED_SLOT = true;
     }
 
     function lock() internal {
-        assembly ("memory-safe") {
-            tstore(IS_UNLOCKED_SLOT, false)
-        }
+        IS_UNLOCKED_SLOT = false;
     }
 
     function isUnlocked() internal view returns (bool unlocked) {
-        assembly ("memory-safe") {
-            unlocked := tload(IS_UNLOCKED_SLOT)
-        }
+        unlocked = IS_UNLOCKED_SLOT;
     }
 }
